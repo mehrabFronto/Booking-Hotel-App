389e65ffedc536c431d703fdbbca7e59da0b5c5e
Mehrabd20 <mehrabd20@gamil.com>
2023-10-17 17:48:19 +0330

    developing bookmarks list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import AppLayout from "./components/AppLayout/AppLayout";
+import Bookmark from "./components/Bookmark/Bookmark";
 import BookmarkLayout from "./components/BookmarkLayout/BookmarkLayout";
 import Header from "./components/Header/Header";
 import HotelsList from "./components/HotelsList/HotelsList";
@@ -38,7 +39,11 @@ function App() {
                   element={<BookmarkLayout />}>
                   <Route
                      index
-                     element={<div>bookmark list...</div>}
+                     element={<Bookmark />}
+                  />
+                  <Route
+                     path=":id"
+                     element={<div>single bookmark</div>}
                   />
                   <Route
                      path="add"
---
+++ src/components/Bookmark/Bookmark.jsx
@@ -0,0 +1,38 @@
+import ReactCountryFlag from "react-country-flag";
+import { Link } from "react-router-dom";
+import { useBookmarks } from "../../contexts/BookmarksListProvider";
+import Loader from "../Loader/Loader";
+
+const Bookmark = () => {
+   const { bookmarks, isLoading } = useBookmarks();
+
+   if (isLoading) return <Loader />;
+
+   return (
+      <div>
+         <h2>Bookmarks List</h2>
+         <div className="bookmarkList">
+            {bookmarks.map((item) => {
+               return (
+                  <Link
+                     key={item.id}
+                     to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`}>
+                     <div className="bookmarkItem">
+                        <ReactCountryFlag
+                           svg
+                           countryCode={item.countryCode}
+                        />
+                        &nbsp;
+                        <strong>{item.cityName}</strong>
+                        &nbsp;
+                        <span>{item.country}</span>
+                     </div>
+                  </Link>
+               );
+            })}
+         </div>
+      </div>
+   );
+};
+
+export default Bookmark;
